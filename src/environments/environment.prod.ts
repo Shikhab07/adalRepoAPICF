6d1b14a58fef1e9db1e42c8832ebaad71a6375ce
Shikha <shika.basra@evry.com>
2019-07-09 19:26:05 +0530

    Added adal to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  adalConfig: {
+    tenant: 'shibas.onmicrosoft.com',
+    clientId: 'd663212d-da0c-49e6-82d0-1140afb9da66',
+    redirectUri: window.location.origin + '/login',
+    cacheLocation: 'localStorage',
+    navigateToLoginRequestUrl: false
+  }
 };
